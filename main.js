2b8e86fae78a33f113348d13f39def0f93ecffb7
Maria Paula Espadari <137806233+mariaapaulari@users.noreply.github.com>
2025-06-24 08:50:03 -0300

    i

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ function diminuiTamanho(){
       tamanhoSenha--;
     }
     numeroSenha.textContent = tamanhoSenha;
+    geraSenha();
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
@@ -26,5 +27,14 @@ console.log(botoes)
 
 const campoSenha = document.querySelector('#campo-senha');
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVXYWZ';
+geraSenha();  
+function geraSenha(){
+    let senha = '' ”;
+    for (let i = 0; i < tamanhoSenha;i++){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+     numeroAleatorio = Math.floor(numeroAleatorio);
+       senha = senha + letrasMaiusculas[numeroAleatorio];
+    }
+}
 
 campoSenha.value = letrasMaiusculas;
